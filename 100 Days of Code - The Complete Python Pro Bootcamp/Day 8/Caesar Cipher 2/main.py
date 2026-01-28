cfadabfd8ff1217a2c166c98fce6c817f1810e8f
brendonHale <garrett.hale88@gmail.com>
2026-01-28 13:15:28 -0700

    Day 17

--- 100 Days of Code - The Complete Python Pro Bootcamp/Day 8/Caesar Cipher 2/main.py
+++ 100 Days of Code - The Complete Python Pro Bootcamp/Day 8/Caesar Cipher 2/main.py
@@ -5,9 +5,9 @@ text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
 
-# TODO-1: Create a function called 'decrypt()' that takes 'original_text' and 'shift_amount' as inputs.
-# TODO-2: Inside the 'decrypt()' function, shift each letter of the 'original_text' *backwards* in the alphabet
-#  by the shift amount and print the decrypted text.
+# TODO-1: Create a function called 'decrypt()' that takes 'original_' and 'shift_amount' as inputs.
+# TODO-2: Inside the 'decrypt()' function, shift each letter of the 'original_' *backwards* in the alphabet
+#  by the shift amount and print the decrypted .
 # TODO-3: Combine the 'encrypt()' and 'decrypt()' functions into one function called 'caesar()'.
 #  Use the value of the user chosen 'direction' variable to determine which functionality to use.
 
